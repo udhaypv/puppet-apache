c9294902d465e7b90d26c561c6f2034fde2f216f
root <root@udhaypv1c.mylabserver.com>
2020-02-19 15:28:00 +0000

    Refactored params.pp in favor of Hoiera

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,11 +3,8 @@
 # A description of what this class does
 #
 
-class apache::install (
-  $install_name = $apache::params::install_name,
-  $install_ensure = $apache::params::install_ensure,
-) inherits apache::params {
-  package { "${install_name}":
-    ensure => $install_ensure,
+class apache::install {
+  package { "${apache::install_name}":
+    ensure => $apache::install_ensure,
   }
 }
